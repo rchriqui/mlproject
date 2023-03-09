7512c58649c96c5debb4bb07eb174671421a8db7
Robinrob92 <robin.chriqui@gmail.com>
2023-03-09 16:13:36 +0200

    changed logger

--- src/logger.py
+++ src/logger.py
@@ -1,10 +1,13 @@
+
 import logging
 import os
 from datetime import datetime
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+
+LOG_FILE = "{}.log".format(datetime.now().strftime('%m_%d_%Y_%H_%M_%S'))
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
+# os.makedirs(logs_path, True)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
@@ -12,3 +15,9 @@ logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
+
+
+)
+
+if __name__=="__main__":
+    logging.info("logging has started")
